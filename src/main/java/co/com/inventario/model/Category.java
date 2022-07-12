8ed8eb9bd581a563be0d8acb81d06422a845ded4
jcvillacobz <jucaviza6@gmail.com>
2022-07-12 17:58:03 -0500

    Comentario Prueba

---
+++ src/main/java/co/com/inventario/model/Category.java
@@ -0,0 +1,29 @@
+package co.com.inventario.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="category")
+public class Category implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5162968496664716615L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String description;
+	
+}
